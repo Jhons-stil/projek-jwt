abe3fcc9df2a7fad7d36c99148834a240f9658dc
Haikal <haikalhakam422@gmail.com>
2026-01-30 21:00:46 +0700

    modifikasi

--- src/app.js
+++ src/app.js
@@ -2,13 +2,18 @@ const express = require("express");
 const routerUser = require("./http/user/router.js");
 const routerBuku = require("./http/buku/router.js");
 
+const routerPembeli = require("./http/pembeli/router.js");
+
 const app = express();
 const PORT = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", routerUser);
-app.use( "/api/buku", routerBuku );
+
+app.use("/api/buku", routerBuku);
+
+app.use("/api/pembeli", routerPembeli);
 
 app.listen(PORT, () => {
   console.log("server Berjalan.................");
